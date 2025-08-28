b5bacfdc57ff70290b29c7ca22de08ceb653d32d
Lasse Kuechler <lasse@ukuechler.de>
2025-08-28 22:01:23 +0200

    feat(searchBookmarksByTitle): improve searching over folder

--- commands/bookmarks/searchBookmarksByTitle/match.js
+++ commands/bookmarks/searchBookmarksByTitle/match.js
@@ -2,60 +2,37 @@ import fuzzysort from "fuzzysort";
 
 import { BookmarkMatchComponent } from "./component.js";
 
+const flattenBookmarks = (nodes, parentTitle = "") =>
+	nodes.flatMap((node) => {
+		const currentTitle = [parentTitle, node.title].filter(Boolean).join(" / ");
+		if (node.children) {
+			return flattenBookmarks(node.children, currentTitle);
+		} else {
+			return [
+				{
+					...node,
+					folderTitle: parentTitle,
+				},
+			];
+		}
+	});
+
 export async function searchBookmarksByTitle(query) {
 	if (!query || !query.startsWith("@")) return [];
 	const searchTerm = query.slice(1).toLowerCase();
 	const allBookmarks = await browser.bookmarks.getTree();
-	const flattenBookmarks = (nodes, parentTitle = "") =>
-		nodes.flatMap((node) => {
-			const currentTitle = node.title || parentTitle;
-			if (node.children) {
-				return flattenBookmarks(node.children, currentTitle);
-			} else {
-				return [{ ...node, folderTitle: parentTitle }];
-			}
-		});
 
 	const flat = flattenBookmarks(allBookmarks);
 
 	const fuseTitle = fuzzysort.go(searchTerm, flat, {
-		key: ["title"],
+		keys: ["title", "folderTitle"],
 	});
-	const titleMatches = fuseTitle
-		.filter(
-			(result) =>
-				result.obj.title &&
-				result.obj.folderTitle !== "Bookmarks Menu" &&
-				result.obj.folderTitle !== "Mozilla Firefox",
-		)
-		.map((result) => ({
-			...result.obj,
-			matchType: "title",
-			score: result.score,
-		}));
+	const titleMatches = fuseTitle.map((result) => ({
+		...result.obj,
+		score: result.score,
+	}));
 
-	const titleIds = new Set(titleMatches.map((b) => b.id));
-
-	const fuseFolderTitle = fuzzysort.go(searchTerm, flat, {
-		key: ["folderTitle"],
-	});
-	const folderTitleMatches = fuseFolderTitle
-		.filter(
-			(result) =>
-				result.obj.folderTitle &&
-				result.obj.url &&
-				result.obj.title &&
-				!titleIds.has(result.obj.id),
-		)
-		.map((result) => ({
-			...result.obj,
-			matchType: "folderTitle",
-			score: result.score,
-		}));
-
-	const allMatches = [...titleMatches, ...folderTitleMatches].sort(
-		(a, b) => b.score - a.score,
-	);
+	const allMatches = [...titleMatches].sort((a, b) => b.score - a.score);
 	return allMatches.map(
 		(bookmark) => new BookmarkMatchComponent(bookmark, bookmark.score),
 	);
